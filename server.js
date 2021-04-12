c6e7a6c8d6077c801759344f753e992c222ab201
Manthan Gupta <manthangupta109@gmail.com>
2021-04-12 14:42:23 +0530

    Basic Project Setup

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000 || process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server Running on port ${PORT}`);
+});

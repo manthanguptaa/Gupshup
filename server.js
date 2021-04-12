2362dd5a32eee235fa1e137863b7635825f21e5f
Manthan Gupta <manthangupta109@gmail.com>
2021-04-12 22:40:12 +0530

    Enhancement: Sending Message Object instead of string

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
+const { format } = require("path");
 
 const app = express();
 
@@ -13,17 +15,23 @@ const io = socketio(server);
 app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", (socket) => {
-  socket.emit("message", "Welcome To GupShup!");
+  socket.emit("message", formatMessage("Gupshup Bot", "Welcome To GupShup!"));
   //Broadcast when a user connects
-  socket.broadcast.emit("message", "A user has joined the chat");
+  socket.broadcast.emit(
+    "message",
+    formatMessage("Gupshup Bot", "A user has joined the chat")
+  );
   //Broadcast when user disconnects
   socket.on("disconnect", () => {
-    io.emit("message", "A user has left the chat");
+    io.emit(
+      "message",
+      formatMessage("Gupshup Bot", "A user has left the chat")
+    );
   });
 
   //Listen for chat message
   socket.on("chatMessage", (msg) => {
-    io.emit("message", msg);
+    io.emit("message", formatMessage("USER", msg));
   });
 });
 
